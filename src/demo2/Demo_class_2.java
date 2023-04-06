95c7bb6af8ade88de9b907f729a5c20df323861f
Rimsky <Rimsky@DESKTOP-BO2IQFC.lan>
2023-04-05 23:28:50 -0400

    "Initial Commit"

---
+++ src/demo2/Demo_class_2.java
@@ -0,0 +1,11 @@
+package demo2;
+
+public class Demo_class_2 {
+	
+	public static void main (String args []) {
+		
+		System.out.println("Demo Class 2");
+		System.out.println("Testing Demo Class 2");
+	}
+
+}
